c1a546e08bd4914e2baf654e39aa016fe7f55bbd
michael.muller1 <michael.muller1@EIN-MUELLERM.ing.intra.eiaj.ch>
2013-03-29 09:40:49 +0100

    prob91

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 
 
+    //Voici la modification faite sur un branche prob91
     //ceci est une nouvelle modification
 	return 0;
 }
