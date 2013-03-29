6b57a4db0fa08bd4719e28dee1832a6c51931669
miiicmueller <miiicmueller@gmail.com>
2013-03-29 10:58:20 +0100

    version de base

--- main.c
+++ main.c
@@ -6,14 +6,5 @@
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 
-
-    //Voici la modification faite sur un branche prob91 2
-    //ceci est une nouvelle modification
-    //Re modif sur la branche master 2
-    // je modisfie cette ligne dans la branche pb100
-    // je modifie aussi cette ligne dans master
-
-//rettest
-
 	return 0;
 }
