f4e3a2566834811448b147a8fa6df1632c669c97
michael.muller1 <michael.muller1@EIN-MUELLERM.ing.intra.eiaj.ch>
2013-03-29 09:42:26 +0100

    test branche

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 
 
-    //Voici la modification faite sur un branche prob91
+    //Voici la modification faite sur un branche prob91 2
     //ceci est une nouvelle modification
 	return 0;
 }
