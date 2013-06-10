2effb9746e0fd8be3b5a1d20c24e6887842a859e
miiicmueller <miiicmueller@gmail.com>
2013-06-10 09:36:48 +0200

    iCpu fonctionnel : 		25MHz MCLK 		4 MHz SMCLK 		32.768 kHz ACLK

--- Interfaces/iCpu.cpp
+++ Interfaces/iCpu.cpp
@@ -3,92 +3,76 @@
 
 #include "iCpu.h"
 
-void iCpu::setPowerMode(powerModeEnum aMode)
-    {
-    }
+void iCpu::setPowerMode(powerModeEnum aMode) {
+}
 
-void iCpu::configFrequency()
-    {
-    volatile unsigned int i;
+void iCpu::configFrequency() {
+	volatile unsigned int i;
 
-    P1DIR |= BIT1;                            // P1.1 output
+	//Selectionner la fct Xt2 sur les IOs
+	P5SEL |= BIT2 + BIT3;                       // Port select XT2
 
-    P1DIR |= BIT0;                            // ACLK set out to pins
-    P1SEL |= BIT0;
-    P2DIR |= BIT2;                            // SMCLK set out to pins
-    P2SEL |= BIT2;
-    P7DIR |= BIT7;                            // MCLK set out to pins
-    P7SEL |= BIT7;
+	// Increase Vcore setting to level3 to support fsystem=25MHz
+	// NOTE: Change core voltage one level at a time..
+	SetVcoreUp(0x01);
+	SetVcoreUp(0x02);
+	SetVcoreUp(0x03);
 
-    //Selectionner la fct Xt2 sur les IOs
-    P5SEL |= BIT2 + BIT3;                       // Port select XT2
+	UCSCTL6 = XT2DRIVE_1 | XT1OFF ;
 
-    // Increase Vcore setting to level3 to support fsystem=25MHz
-    // NOTE: Change core voltage one level at a time..
-    SetVcoreUp(0x01);
-    SetVcoreUp(0x02);
-    SetVcoreUp(0x03);
+	UCSCTL3 = SELREF_2;               // Set DCO FLL reference = REFO
+	UCSCTL4 |= SELA_2 ;	// Set ACLK = REFO
+	//UCSCTL6 = XT2DRIVE_1 | XT2BYPASS | XT1OFF;//activation de xt2 et désactivation de xt1
+	//UCSCTL6 = XT2DRIVE_1 | XT1OFF ;   //activation de xt2 et désactivation de xt1
 
-    UCSCTL3 = SELREF_2;               // Set DCO FLL reference = REFO
+	__bis_SR_register(SCG0);
+	// Disable the FLL control loop
+	UCSCTL0 = 0x0000;                 // Set lowest possible DCOx, MODx
+	UCSCTL1 = DCORSEL_7;              // Select DCO range 50MHz operation
+	UCSCTL2 = FLLD_0 + 762;           // Set DCO Multiplier for 25MHz
+	// (N + 1) * FLLRef = Fdco
+	// (762 + 1) * 32768 = 25MHz
+	// Set FLL Div = fDCOCLK/2
+	__bic_SR_register(SCG0);
+	// Enable the FLL control loop
 
-    __bis_SR_register(SCG0);
-    // Disable the FLL control loop
-    UCSCTL0 = 0x0000;                 // Set lowest possible DCOx, MODx
-    UCSCTL1 = DCORSEL_7;              // Select DCO range 50MHz operation
-    UCSCTL2 = FLLD_0 + 762;           // Set DCO Multiplier for 25MHz
-				      // (N + 1) * FLLRef = Fdco
-				      // (762 + 1) * 32768 = 25MHz
-				      // Set FLL Div = fDCOCLK/2
-    __bic_SR_register(SCG0);
-    // Enable the FLL control loop
+	// Worst-case settling time for the DCO when the DCO range bits have been
+	// changed is n x 32 x 32 x f_MCLK / f_FLL_reference. See UCS chapter in 5xx
+	// UG for optimization.
+	// 32 x 32 x 25 MHz / 32,768 Hz ~ 780k MCLK cycles for DCO to settle
+	__delay_cycles(782000);
 
-    // Worst-case settling time for the DCO when the DCO range bits have been
-    // changed is n x 32 x 32 x f_MCLK / f_FLL_reference. See UCS chapter in 5xx
-    // UG for optimization.
-    // 32 x 32 x 25 MHz / 32,768 Hz ~ 780k MCLK cycles for DCO to settle
-    __delay_cycles(782000);
+	// Loop until XT1,XT2 & DCO stabilizes - In this case only DCO has to stabilize
+	do {
+		UCSCTL7 &= ~(XT2OFFG + XT1LFOFFG + DCOFFG);
+		// Clear XT2,XT1,DCO fault flags
+		SFRIFG1 &= ~OFIFG;                      // Clear fault flags
+	} while (SFRIFG1 & OFIFG);                   // Test oscillator fault flag
 
-    // Loop until XT1,XT2 & DCO stabilizes - In this case only DCO has to stabilize
-    do
-	{
-	UCSCTL7 &= ~(XT2OFFG + XT1LFOFFG + DCOFFG);
-	// Clear XT2,XT1,DCO fault flags
-	SFRIFG1 &= ~OFIFG;                      // Clear fault flags
-	}
-    while (SFRIFG1 & OFIFG);                   // Test oscillator fault flag
+	UCSCTL5 = 0x0000;	//all dividers divide by 1
+	UCSCTL4 |= SELS_5 | SELM_4;	// Set ACLK = REFO
+	// Set SMCLK = XT2CLK
+	// Set MCLK = DCOCLK
+}
 
-    UCSCTL5 = 0x0000;	//all dividers divide by 1
-    UCSCTL6 = XT2DRIVE_1 | XT2BYPASS | XT1OFF;//activation de xt2 et désactivation de xt1
-    UCSCTL4 = SELA_2 | SELS_5 | SELM_3;	// Set ACLK = REFO
-					// Set SMCLK = XT2CLK
-					// Set MCLK = DCOCLK
-
-    while (1)
-	{
-	P1OUT ^= BIT1;                          // Toggle P1.1
-	__delay_cycles(600000);                 // Delay
-	}
-    }
-
-void iCpu::SetVcoreUp(unsigned int level)
-    {
-    // Open PMM registers for write
-    PMMCTL0_H = PMMPW_H;
-    // Set SVS/SVM high side new level
-    SVSMHCTL = SVSHE + SVSHRVL0 * level + SVMHE + SVSMHRRL0 * level;
-    // Set SVM low side to new level
-    SVSMLCTL = SVSLE + SVMLE + SVSMLRRL0 * level;
-    // Wait till SVM is settled
-    while ((PMMIFG & SVSMLDLYIFG)== 0);
-    // Clear already set flags
-    PMMIFG &= ~(SVMLVLRIFG + SVMLIFG);
-    // Set VCore to new level
-    PMMCTL0_L = PMMCOREV0 * level;
-    // Wait till new level reached
-    if ((PMMIFG & SVMLIFG))
-	while ((PMMIFG & SVMLVLRIFG)== 0);
-    // Set SVS/SVM low side to new level
-    SVSMLCTL = SVSLE + SVSLRVL0 * level + SVMLE + SVSMLRRL0 * level;
-    // Lock PMM registers for write access
-    PMMCTL0_H = 0x00;
-    }
+void iCpu::SetVcoreUp(unsigned int level) {
+	// Open PMM registers for write
+	PMMCTL0_H = PMMPW_H;
+	// Set SVS/SVM high side new level
+	SVSMHCTL = SVSHE + SVSHRVL0 * level + SVMHE + SVSMHRRL0 * level;
+	// Set SVM low side to new level
+	SVSMLCTL = SVSLE + SVMLE + SVSMLRRL0 * level;
+	// Wait till SVM is settled
+	while ((PMMIFG & SVSMLDLYIFG)== 0);
+	// Clear already set flags
+	PMMIFG &= ~(SVMLVLRIFG + SVMLIFG);
+	// Set VCore to new level
+	PMMCTL0_L = PMMCOREV0 * level;
+	// Wait till new level reached
+	if ((PMMIFG & SVMLIFG))
+		while ((PMMIFG & SVMLVLRIFG)== 0);
+	// Set SVS/SVM low side to new level
+	SVSMLCTL = SVSLE + SVSLRVL0 * level + SVMLE + SVSMLRRL0 * level;
+	// Lock PMM registers for write access
+	PMMCTL0_H = 0x00;
+}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <msp430.h>
 #include "Modules/mGSM.h"
+#include "Modules/mCpu.h"
 #include "Interfaces/iDIO.h"
 #include "Interfaces/iUART.h"
 #include "Tools/tCommandesAT.h"
@@ -15,8 +16,8 @@ int _system_pre_init(void) {
 	return (1);
 }
 
-//Function declarations
-void Init_Clock(void);
+//void Init_Clock(void);
+//void SetVcoreUp(unsigned int level);
 
 int main(void) {
 
@@ -25,7 +26,17 @@ int main(void) {
 
 	WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 
-	Init_Clock();
+	//Init_Clock();
+	P1DIR |= BIT1;                            // P1.1 output
+
+	P1DIR |= BIT0;                            // ACLK set out to pins
+	P1SEL |= BIT0;
+	P2DIR |= BIT2;                            // SMCLK set out to pins
+	P2SEL |= BIT2;
+	P7DIR |= BIT7;                            // MCLK set out to pins
+	P7SEL |= BIT7;
+
+	mCpu::configFrequency();
 
 	__bis_SR_register(GIE);
 	// Enter LPM0, interrupts enabled
@@ -39,45 +50,85 @@ int main(void) {
 
 	iUart.enable();
 
-//	while(iUart.availableCharToRead()<=5);
-//	iUart.readLine(smsReceived);
+	iUart.write('A');
 
-	mGSM mGsm(&iDio, &iUart, &tComAt);
-	mGsm.mSetup();
-	mGsm.mOpen();
+//	mGSM mGsm(&iDio, &iUart, &tComAt);
+//	mGsm.mSetup();
+//	mGsm.mOpen();
 
-	//hasSMSRecu = mGsm.getSMS(smsReceived); // prend le sms toujours le suivant
-
-	bool banane = hasSMSRecu;
-
+	while (1) {
+		P1OUT ^= BIT1;                          // Toggle P1.1
+		__delay_cycles(600000);                 // Delay
+	}
 }
 
 /*
  * ======== Init_Clock ========
  */
-void Init_Clock(void) {
-
-	//Configuration de la fr�quence
-	//Selectionner la fct Xt2 sur les IOs
-	P5SEL |= BIT2 + BIT3;                       // Port select XT2
-
-	UCSCTL6 &= ~XT2OFF;                       // Enable XT2
-	UCSCTL3 |= SELREF_2;                      // FLLref = REFO
-	// Since LFXT1 is not used,
-	// sourcing FLL with LFXT1 can cause
-	// XT1OFFG flag to set
-	UCSCTL4 |= SELA_2;                        // ACLK=REFO,SMCLK=DCO,MCLK=DCO
-
-	// Loop until XT1,XT2 & DCO stabilizes - in this case loop until XT2 settles
-	do {
-		UCSCTL7 &= ~(XT2OFFG + XT1LFOFFG + DCOFFG);
-		// Clear XT2,XT1,DCO fault flags
-		SFRIFG1 &= ~OFIFG;                      // Clear fault flags
-	} while (SFRIFG1 & OFIFG);                   // Test oscillator fault flag
-
-	UCSCTL6 &= ~XT2DRIVE0;                    // Decrease XT2 Drive according to
-	// expected frequency
-	UCSCTL4 |= SELS_5 + SELM_5;               // SMCLK=MCLK=XT2
-
-}
-
+//void Init_Clock(void) {
+//
+//	P1DIR |= BIT1;                            // P1.1 output
+//
+//	P1DIR |= BIT0;                            // ACLK set out to pins
+//	P1SEL |= BIT0;
+//	P2DIR |= BIT2;                            // SMCLK set out to pins
+//	P2SEL |= BIT2;
+//	P7DIR |= BIT7;                            // MCLK set out to pins
+//	P7SEL |= BIT7;
+//
+//	// Increase Vcore setting to level3 to support fsystem=25MHz
+//	// NOTE: Change core voltage one level at a time..
+//	SetVcoreUp(0x01);
+//	SetVcoreUp(0x02);
+//	SetVcoreUp(0x03);
+//
+//	UCSCTL3 = SELREF_2;                       // Set DCO FLL reference = REFO
+//	UCSCTL4 |= SELA_2;                        // Set ACLK = REFO
+//
+//	__bis_SR_register(SCG0);
+//// Disable the FLL control loop
+//	UCSCTL0 = 0x0000;                         // Set lowest possible DCOx, MODx
+//	UCSCTL1 = DCORSEL_7;                     // Select DCO range 50MHz operation
+//	UCSCTL2 = FLLD_0 + 762;                   // Set DCO Multiplier for 25MHz
+//											  // (N + 1) * FLLRef = Fdco
+//											  // (762 + 1) * 32768 = 25MHz
+//											  // Set FLL Div = fDCOCLK/2
+//	__bic_SR_register(SCG0);
+//// Enable the FLL control loop
+//
+//// Worst-case settling time for the DCO when the DCO range bits have been
+//// changed is n x 32 x 32 x f_MCLK / f_FLL_reference. See UCS chapter in 5xx
+//// UG for optimization.
+//// 32 x 32 x 25 MHz / 32,768 Hz ~ 780k MCLK cycles for DCO to settle
+//	__delay_cycles(782000);
+//
+//// Loop until XT1,XT2 & DCO stabilizes - In this case only DCO has to stabilize
+//	do {
+//		UCSCTL7 &= ~(XT2OFFG + XT1LFOFFG + DCOFFG);
+//		// Clear XT2,XT1,DCO fault flags
+//		SFRIFG1 &= ~OFIFG;                                  // Clear fault flags
+//	} while (SFRIFG1 & OFIFG);                   // Test oscillator fault flag
+//
+//}
+//
+//void SetVcoreUp(unsigned int level) {
+//	// Open PMM registers for write
+//	PMMCTL0_H = PMMPW_H;
+//	// Set SVS/SVM high side new level
+//	SVSMHCTL = SVSHE + SVSHRVL0 * level + SVMHE + SVSMHRRL0 * level;
+//	// Set SVM low side to new level
+//	SVSMLCTL = SVSLE + SVMLE + SVSMLRRL0 * level;
+//	// Wait till SVM is settled
+//	while ((PMMIFG & SVSMLDLYIFG)== 0);
+//	// Clear already set flags
+//	PMMIFG &= ~(SVMLVLRIFG + SVMLIFG);
+//	// Set VCore to new level
+//	PMMCTL0_L = PMMCOREV0 * level;
+//	// Wait till new level reached
+//	if ((PMMIFG & SVMLIFG))
+//		while ((PMMIFG & SVMLVLRIFG)== 0);
+//	// Set SVS/SVM low side to new level
+//	SVSMLCTL = SVSLE + SVSLRVL0 * level + SVMLE + SVSMLRRL0 * level;
+//	// Lock PMM registers for write access
+//	PMMCTL0_H = 0x00;
+//}
